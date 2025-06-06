62558ba7212f2f22c8f1af1ed117d5fd13fa7afc
felipehoch <hochfelipe@gmail.com>
2025-06-06 00:52:55 -0300

    feat: :sparkles: Add Kafka with SASL/SHA-256

---
+++ init-kafka.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Aguardando Kafka iniciar..."
+sleep 10
+
+echo "Criando usuário SCRAM-SHA-256 no Kafka..."
+kafka-configs --zookeeper zookeeper:2181 \
+  --alter \
+  --add-config 'SCRAM-SHA-256=[iterations=4096,password=kafka-secret]' \
+  --entity-type users \
+  --entity-name kafka
+
+echo "Inicializando Kafka..."
+/etc/confluent/docker/run
